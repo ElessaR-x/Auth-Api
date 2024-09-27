3de42814ccfb953e4f3079b9607fbfd203fa7dda
Arda <mehmetardaozger@gmail.com>
2024-09-28 01:19:56 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
+    
