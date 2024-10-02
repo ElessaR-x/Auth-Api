fdc3d481d39d5976be063b753bf2d5d712ed37f3
ElessaR-x <mehmetardaozger@gmail.com>
2024-10-03 01:20:18 +0300

    Log system added

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from src.Admin.admin import router as admin_router
 from src.Reseller.reseller import router as reseller_router
 from src.Owner.owner import router as owner_router
 from src.Auth.auth import router as auth_router
 from src.Customer.customer import router as customer_router
+import uvicorn
+import logging
+import time
+
+# Logging ayarlarını yapılandırma
+logging.basicConfig(
+    filename='app.log',  # Log dosyasının adı
+    level=logging.INFO,  # Log seviyesi
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Log formatı
+)
 
 app = FastAPI()
 
@@ -14,7 +24,16 @@ app.include_router(reseller_router, prefix="/reseller", tags=["Reseller"])
 app.include_router(owner_router, prefix="/owner", tags=["Owner"])
 app.include_router(customer_router, prefix="/customer", tags=["Customer"])
 
-if __name__ == "__main__":
-    import uvicorn
+# Request middleware'i ekleyerek her isteği loglama
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    # İstek zamanı ve URL bilgilerini alalım
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    log_details = f"METHOD: {request.method} - URL: {request.url} - STATUS CODE: {response.status_code} - PROCESS TIME: {process_time:.2f}s"
+    logging.info(log_details)
+    return response
 
+if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
